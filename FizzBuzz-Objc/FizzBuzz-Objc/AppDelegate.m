7fef52e09ba14c96c734b8013ef3b8f0ec9c4593
christianmonson <christianrmonson@gmail.com>
2015-01-31 15:00:52 -0700

    completes "fizzbuzz"

--- FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
+++ FizzBuzz-Objc/FizzBuzz-Objc/AppDelegate.m
@@ -10,12 +10,43 @@
 
 @implementation AppDelegate
 
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    // Insert code here
-    
-    return YES;
+    for (int i = 1; i < 101; i++) {
+        BOOL buzzed = [self isBuzzed:i];
+        BOOL fizzed = [self isFizzed:i];
+        
+        if (buzzed && !fizzed) {
+            NSLog (@"buzz");
+        } else if (!buzzed && fizzed) {
+            NSLog(@"fizz");
+        } else if (buzzed && fizzed) {
+            NSLog(@"fizzbuzz");
+        } else {
+            NSLog(@"%d", i);
+            }
+            
+        }
+    return TRUE;
+}
+              
+-(BOOL)isBuzzed:(int)number {
+    if (number % 3 ==0) {
+        return TRUE;
+    } else if ([[NSString stringWithFormat:@"%d", number] containsString:@"3"]) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
+
+-(BOOL)isFizzed:(int)number {
+    if (number % 5 ==0) {
+        return TRUE;
+    } else if ([[NSString stringWithFormat:@"%d", number] containsString:@"5"]) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
 }
 
 @end
